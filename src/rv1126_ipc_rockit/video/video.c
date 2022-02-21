9fd046cb3a585194de3b502df988e5e2cac9e225
Fenrir Lin <fenrir.lin@rock-chips.com>
2022-02-21 20:03:12 +0800

    RK_MPI_VENC_GetStream change the timeout to 20 s
    Signed-off-by: Fenrir Lin <fenrir.lin@rock-chips.com>
    Change-Id: Iabc85f869376e33ada69e1e762d2efc3e99af184

--- src/rv1126_ipc_rockit/video/video.c
+++ src/rv1126_ipc_rockit/video/video.c
@@ -121,7 +121,7 @@ static void *rkipc_get_venc_0(void *arg) {
 
 	while (g_video_run_) {
 		// 5.get the frame
-		ret = RK_MPI_VENC_GetStream(VIDEO_PIPE_0, &stFrame, 1000);
+		ret = RK_MPI_VENC_GetStream(VIDEO_PIPE_0, &stFrame, 20000);
 		if (ret == RK_SUCCESS) {
 			void *data = RK_MPI_MB_Handle2VirAddr(stFrame.pstPack->pMbBlk);
 			// fwrite(data, 1, stFrame.pstPack->u32Len, fp);
@@ -177,7 +177,7 @@ static void *rkipc_get_venc_1(void *arg) {
 
 	while (g_video_run_) {
 		// 5.get the frame
-		ret = RK_MPI_VENC_GetStream(VIDEO_PIPE_1, &stFrame, 1000);
+		ret = RK_MPI_VENC_GetStream(VIDEO_PIPE_1, &stFrame, 20000);
 		if (ret == RK_SUCCESS) {
 			void *data = RK_MPI_MB_Handle2VirAddr(stFrame.pstPack->pMbBlk);
 			// LOG_INFO("Count:%d, Len:%d, PTS is %" PRId64", enH264EType is %d\n", loopCount,
@@ -227,7 +227,7 @@ static void *rkipc_get_venc_2(void *arg) {
 
 	while (g_video_run_) {
 		// 5.get the frame
-		ret = RK_MPI_VENC_GetStream(VIDEO_PIPE_2, &stFrame, 1000);
+		ret = RK_MPI_VENC_GetStream(VIDEO_PIPE_2, &stFrame, 20000);
 		if (ret == RK_SUCCESS) {
 			void *data = RK_MPI_MB_Handle2VirAddr(stFrame.pstPack->pMbBlk);
 			// LOG_INFO("Count:%d, Len:%d, PTS is %" PRId64", enH264EType is %d\n", loopCount,
@@ -282,7 +282,7 @@ static void *rkipc_get_jpeg(void *arg) {
 		if (!take_photo_one)
 			continue;
 		// 5.get the frame
-		ret = RK_MPI_VENC_GetStream(JPEG_VENC_CHN, &stFrame, 1000);
+		ret = RK_MPI_VENC_GetStream(JPEG_VENC_CHN, &stFrame, 20000);
 		if (ret == RK_SUCCESS) {
 			void *data = RK_MPI_MB_Handle2VirAddr(stFrame.pstPack->pMbBlk);
 			LOG_INFO("Count:%d, Len:%d, PTS is %" PRId64 ", enH264EType is %d\n", loopCount,
