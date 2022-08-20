45dec81371f44047b458b6049eb4303857584cfe
Fenrir Lin <fenrir.lin@rock-chips.com>
2022-08-20 17:02:18 +0800

    rv1106_ipc: fix spelling problem with rotation
    Signed-off-by: Fenrir Lin <fenrir.lin@rock-chips.com>
    Change-Id: If3d6315008d04eedab74e07d6ac2a47bc43cc636

--- src/rv1106_ipc/video/video.c
+++ src/rv1106_ipc/video/video.c
@@ -2550,7 +2550,7 @@ int rk_video_set_jpeg_resolution(char **value) {
 
 int rk_video_get_rotation(int *value) {
 	char entry[128] = {'\0'};
-	snprintf(entry, 127, "video.source:rotaion");
+	snprintf(entry, 127, "video.source:rotation");
 	*value = rk_param_get_int(entry, 0);
 
 	return 0;
@@ -2560,7 +2560,7 @@ int rk_video_set_rotation(int value) {
 	int ret = 0;
 	int rotation = 0;
 	char entry[128] = {'\0'};
-	snprintf(entry, 127, "video.source:rotaion");
+	snprintf(entry, 127, "video.source:rotation");
 	rk_param_set_int(entry, value);
 	if (value == 0) {
 		rotation = ROTATION_0;
