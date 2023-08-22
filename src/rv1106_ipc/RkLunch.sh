edd0e5e640f6e5d7cafe787b3cff0effd662ec58
HongLin <helin.lan@rock-chips.com>
2023-08-22 15:29:24 +0800

    rv1106-ipc: add support 4k config
    Change-Id: I6b38d4cafc62fc43e05c2581c65b0dcad86505c3
    Signed-off-by: HongLin <helin.lan@rock-chips.com>

--- src/rv1106_ipc/RkLunch.sh
+++ src/rv1106_ipc/RkLunch.sh
@@ -90,6 +90,10 @@ post_chk()
 		if [ $? -eq 0 ] ;then
 			ln -s -f /oem/usr/share/rkipc-300w.ini $default_rkipc_ini
 		fi
+		lsmod | grep imx415
+		if [ $? -eq 0 ] ;then
+			ln -s -f /oem/usr/share/rkipc-800w.ini $default_rkipc_ini
+		fi
 	fi
 	tmp_md5=/tmp/.rkipc-ini.md5sum
 	data_md5=/userdata/.rkipc-default.md5sum
