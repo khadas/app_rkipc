87821137ea774d8988af6a68a40715b95c808b91
Yu Zheng <yu.zheng@rock-chips.com>
2024-01-15 18:31:12 +0800

    rv1126: fix memory hole for malloc
    Signed-off-by: yu.zheng <yu.zheng@rock-chips.com>
    Change-Id: I6e30529ed0017dd902f4f6ae9e43b87ea74c9f70

--- src/rv1126_aiisp_ipc/video/video.c
+++ src/rv1126_aiisp_ipc/video/video.c
@@ -3031,6 +3031,7 @@ int rk_video_restart() {
 	ret = rk_storage_deinit();
 	ret = rk_video_deinit();
 	rk_isp_deinit(0);
+	malloc_trim(0);
 	usleep(100 * 1000);
 	rk_isp_init(0, rkipc_iq_file_path_);
 	ret |= rk_video_init();
