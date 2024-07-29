6c45dab1e4a5c945976e906e05cd83e8fed9c519
LiuDiMing Lin <fenrir.lin@rock-chips.com>
2024-07-29 09:16:43 +0800

    rv1106_ipc: add video full range flag
    Signed-off-by: LiuDiMing Lin <fenrir.lin@rock-chips.com>
    Change-Id: I65788657fab3607f14bdce135448bd5f1f61b781

--- src/rv1106_ipc/video/video.c
+++ src/rv1106_ipc/video/video.c
@@ -1039,6 +1039,18 @@ int rkipc_pipe_0_init() {
 	lambds_s.u32Lambda = rk_param_get_int("video.0:lambds", 4);
 	RK_MPI_VENC_SetLambda(VIDEO_PIPE_0, &lambds_s);
 
+	if (!strcmp(tmp_output_data_type, "H.264")) {
+		VENC_H264_VUI_S pstH264Vui;
+		RK_MPI_VENC_GetH264Vui(VIDEO_PIPE_0, &pstH264Vui);
+		pstH264Vui.stVuiVideoSignal.video_full_range_flag = rk_param_get_int("video.0:video_full_range_flag", 1);
+		RK_MPI_VENC_SetH264Vui(VIDEO_PIPE_0, &pstH264Vui);
+	} else if (!strcmp(tmp_output_data_type, "H.265")) {
+		VENC_H265_VUI_S pstH265Vui;
+		RK_MPI_VENC_GetH265Vui(VIDEO_PIPE_0, &pstH265Vui);
+		pstH265Vui.stVuiVideoSignal.video_full_range_flag = rk_param_get_int("video.0:video_full_range_flag", 1);
+		RK_MPI_VENC_SetH265Vui(VIDEO_PIPE_0, &pstH265Vui);
+	}
+
 	VENC_CHN_REF_BUF_SHARE_S stVencChnRefBufShare;
 	memset(&stVencChnRefBufShare, 0, sizeof(VENC_CHN_REF_BUF_SHARE_S));
 	stVencChnRefBufShare.bEnable = rk_param_get_int("video.0:enable_refer_buffer_share", 0);
@@ -1415,6 +1427,18 @@ int rkipc_pipe_1_init() {
 	RK_MPI_VENC_GetLambda(VIDEO_PIPE_1, &lambds_s);
 	lambds_s.u32Lambda = rk_param_get_int("video.1:lambds", 4);
 	RK_MPI_VENC_SetLambda(VIDEO_PIPE_1, &lambds_s);
+
+	if (!strcmp(tmp_output_data_type, "H.264")) {
+		VENC_H264_VUI_S pstH264Vui;
+		RK_MPI_VENC_GetH264Vui(VIDEO_PIPE_1, &pstH264Vui);
+		pstH264Vui.stVuiVideoSignal.video_full_range_flag = rk_param_get_int("video.1:video_full_range_flag", 1);
+		RK_MPI_VENC_SetH264Vui(VIDEO_PIPE_1, &pstH264Vui);
+	} else if (!strcmp(tmp_output_data_type, "H.265")) {
+		VENC_H265_VUI_S pstH265Vui;
+		RK_MPI_VENC_GetH265Vui(VIDEO_PIPE_1, &pstH265Vui);
+		pstH265Vui.stVuiVideoSignal.video_full_range_flag = rk_param_get_int("video.1:video_full_range_flag", 1);
+		RK_MPI_VENC_SetH265Vui(VIDEO_PIPE_1, &pstH265Vui);
+	}
 
 	VENC_CHN_REF_BUF_SHARE_S stVencChnRefBufShare;
 	memset(&stVencChnRefBufShare, 0, sizeof(VENC_CHN_REF_BUF_SHARE_S));
